57e906ae71f5c4ce8eefea808b5e71af31c2e169
rahulg3520 <rahul@nestgroup.net>
2022-11-25 11:11:35 +0530

    data binding

--- src/app/view-emp/view-emp.component.ts
+++ src/app/view-emp/view-emp.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-emp.component.css']
 })
 export class ViewEmpComponent {
+  
+  data:any=[
+    {
+    "name":"Rahul",
+    "designation":"Trainee",
+    "salary":25000,
+    "emailid":"rahul3521",
+    "companyName":"qwerty"
+   },
+   {
+    "name":"Reshma",
+    "designation":"Trainee",
+    "salary":25000,
+    "emailid":"resh521",
+    "companyName":"xyz"
+   },
+   {
+    "name":"Ron",
+    "designation":"Trainee",
+    "salary":25000,
+    "emailid":"ron21",
+    "companyName":"qwerty"
+   },
+   {
+    "name":"Rinku",
+    "designation":"Trainee",
+    "salary":25000,
+    "emailid":"rinku3521",
+    "companyName":"qwerty"
+   },
+   {
+    "name":"Rihana",
+    "designation":"Trainee",
+    "salary":25000,
+    "emailid":"rihana3521",
+    "companyName":"abc"
+   }
 
+ ]
 }
